fc5d305fe17fa2f5fb642d0c1a4e06205f900834
SaeedIsa <saeed.isa90@gmail.com>
2021-02-22 21:12:58 +0200

    math examples

---
+++ math/math_examples.py
@@ -0,0 +1,25 @@
+import math
+
+# Mathematical constants
+print('Pi:', math.pi)
+print('E:', math.e)
+print('Inf:', math.inf)
+print('Tau:', math.tau)
+print('NAN:', math.nan)
+
+# Mathematical tasks
+print('Abs:', math.fabs(-5678))
+print('SQRT:', math.sqrt(25))
+print('Pow:', math.pow(10, 2))
+print('Ceil:', math.ceil(9.123))
+print('Floor:', math.floor(9.123))
+print('Sin:', math.sin(0))
+print('Cos:', math.cos(0))
+print('Tan:', math.tan(0))
+print('Logarithm:', math.log(100))
+print('Logarithm2:', math.log2(100))
+print('Logarithm10:', math.log10(100))
+print('Exp:', math.exp(5))
+print('Factorial:', math.factorial(4))
+
+# help(math)
