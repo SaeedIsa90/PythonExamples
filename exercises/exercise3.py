2f721cd2b40af0df4c79a160a77b4f6ed705f377
SaeedIsa <saeed.isa90@gmail.com>
2021-03-05 12:04:26 +0200

    exercise 3

---
+++ exercises/exercise3.py
@@ -0,0 +1,17 @@
+def check_if_prime(item):
+    for i in range(2, item//2):
+        if (item%i) == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    lst = [1, 11, 13, 25, 217, 2, 3, 10, 5, 7, 11, 23, 29, 31, 43, 47, 100, 102, 1004]
+    prime_lst = []
+
+    for item in lst:
+        if check_if_prime(item):
+            prime_lst.append(item)
+
+    print('Prime lst is:')
+    print(prime_lst)
