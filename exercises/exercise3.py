5727470c0ebf1d2e2c2c1ea52c166530345b4784
SaeedIsa <saeed.isa90@gmail.com>
2021-03-22 11:27:58 +0200

    enhance prime

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,12 +1,12 @@
 def check_if_prime(item):
-    for i in range(2, item//2):
+    for i in range(2, (item//2)+1):
         if (item%i) == 0:
             return False
     return True
 
 
 if __name__ == '__main__':
-    lst = [1, 11, 13, 25, 217, 2, 3, 10, 5, 7, 11, 23, 29, 31, 43, 47, 100, 102, 1004]
+    lst = [1, 4, 11, 13, 25, 217, 2, 3, 10, 5, 7, 11, 23, 29, 31, 43, 47, 100, 102, 1004]
     prime_lst = []
 
     for item in lst:
